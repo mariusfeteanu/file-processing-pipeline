90bc9d5a90ede4403f7f65f60a74890fab54bacc
Marius Feteanu <marius.feteanu@gmail.com>
2019-08-08 06:13:19 +0100

    re-order some functions

--- file_processing_pipeline/schema.py
+++ file_processing_pipeline/schema.py
@@ -13,40 +13,6 @@ from pandas_schema.validation import \
 from file_processing_pipeline import schemas
 
 
-def load_raw_fields_schema(schema_name):
-    query_template = pkg_resources.read_text(schemas, f'{schema_name}.yml')
-    return yaml.safe_load(query_template)['schema']['fields']
-
-
-def parse_validation(validation, reference_sets):
-    def validate_reference_set(reference_name):
-        def validate(series):
-            reference_set = reference_sets[reference_name]
-            return series.isin(reference_set)
-        return CustomSeriesValidation(validate, f'is not a valid {reference_name}')
-
-    if isinstance(validation, dict):
-        name, config = list(validation.items())[0]
-        if name == 'InRangeValidation':
-            return InRangeValidation(**config)
-        elif name == 'reference':
-            return validate_reference_set(config)
-
-
-def get_all_validations(field_schema, reference_sets):
-    for validation in field_schema.get('validations', []):
-        yield parse_validation(validation, reference_sets)
-
-    if not field_schema.get('nullable', True):
-        yield not_null()
-
-    if field_schema['type'] == 'decimal':
-        yield CanConvertValidation(D.Decimal)
-    else:
-        type_map = {'str': str, 'int64': int, 'float64': float, 'bool': bool}
-        yield CanConvertValidation(type_map[field_schema['type']])
-
-
 def load_schema(schema_name, reference_sets):
     raw_fields_schema = load_raw_fields_schema(schema_name)
     schema = Namespace()
@@ -64,6 +30,40 @@ def load_schema(schema_name, reference_sets):
         in raw_fields_schema
     ])
     return schema
+
+
+def load_raw_fields_schema(schema_name):
+    query_template = pkg_resources.read_text(schemas, f'{schema_name}.yml')
+    return yaml.safe_load(query_template)['schema']['fields']
+
+
+def get_all_validations(field_schema, reference_sets):
+    for validation in field_schema.get('validations', []):
+        yield parse_explicit_validation(validation, reference_sets)
+
+    if not field_schema.get('nullable', True):
+        yield not_null()
+
+    if field_schema['type'] == 'decimal':
+        yield CanConvertValidation(D.Decimal)
+    else:
+        type_map = {'str': str, 'int64': int, 'float64': float, 'bool': bool}
+        yield CanConvertValidation(type_map[field_schema['type']])
+
+
+def parse_explicit_validation(validation, reference_sets):
+    def validate_reference_set(reference_name):
+        def validate(series):
+            reference_set = reference_sets[reference_name]
+            return series.isin(reference_set)
+        return CustomSeriesValidation(validate, f'is not a valid {reference_name}')
+
+    if isinstance(validation, dict):
+        name, config = list(validation.items())[0]
+        if name == 'InRangeValidation':
+            return InRangeValidation(**config)
+        elif name == 'reference':
+            return validate_reference_set(config)
 
 
 def not_null():
